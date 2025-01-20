43c193e34dd9f9446b273e1b22469689c9dc18a6
Luis Gustavo <luisgustavomacedo13@gmail.com>
2025-01-20 08:08:41 -0300

    sort function

--- ga.py
+++ ga.py
@@ -40,6 +40,16 @@ def bounds(c, varmin, varmax):
   c['position'] = np.minimum(c['position'], varmax)
   return c['position']
 
+def sort(arr):
+    n = len(arr)
+
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if arr[j]['cost'] > arr[j+1]['cost']:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+    return arr
+
 def ga(
     # problem definition
     costfunc,
