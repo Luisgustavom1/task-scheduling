7046d5f0720e1c06c7a49e441a5d8d27e77d09af
Luis Gustavo <luisgustavomacedo13@gmail.com>
2025-01-25 21:08:46 -0300

    roullette and crossover

--- simple-math-eq.py
+++ simple-math-eq.py
@@ -1,8 +1,43 @@
-
 import numpy as np
+import copy
 
 def combination(a, b, c, d):
     return abs((a + 2*b + 3*c + 4*d) - 30)
+
+def roullette_wheel_selection(probs, pop):
+  c = np.cumsum(probs)
+  ra = np.random.rand(4)
+  newpop = {}
+  for i, r in enumerate(ra):
+    w = np.argwhere(r <= c)
+    pos = w[0][0]    
+    newpop[i] = pop[pos]
+  return newpop
+
+def crossover_selection(pop, crossrate):
+  i = 0
+  selection = []
+  while(i < len(pop)):
+    r = np.random.rand();
+    if(r < crossrate):
+      selection.append(pop[i])
+    i+=1
+  return selection
+
+def crossover(p1, p2, l):
+  r = np.random.randint(0, l - 1) + 1
+  return np.concatenate([p1['solution'][:r], p2['solution'][r:]])
+
+def mutate(population, npop, numvar, mrate):
+  totalgen = npop + numvar
+  rate = round(totalgen * rate, 0)
+  
+  for _ in rate:
+    r = np.random.randint(1, totalgen)
+    ind = round(r / numvar)
+    gen = r % numvar
+    population[ind]['solution'][gen] = np.random.randint(0, 30)
+
 
 def ga(
     costfunc, 
@@ -12,6 +47,8 @@ def ga(
     minbound,
     maxbound,
     generations,
+    crossrate,
+    mrate
 ):
     population = {}
 
@@ -25,14 +62,29 @@ def ga(
         costs = []
 
         for i in range(npop):
-            c = costfunc(population[i])
+            ch = population[i]['solution']
+            c = 1 / (1 + costfunc(ch[0], ch[1], ch[2], ch[3]))
             population[i]['cost'] = c
-            costs.push(c)
-            t = t + (1/(1+c))
+            costs.append(c)
+            t = t + c
 
         costs = np.array(costs)
-        probs = costs / t 
+        probs = costs / t
         
-        print(probs)
+        newpop = roullette_wheel_selection(probs, population)
 
-ga(combination, 6, 1, 4, 0, 30, 6)
+        parents = crossover_selection(newpop, crossrate)
+        lparents = len(parents)
+        if lparents <= 1:
+          continue
+
+        for i, p in enumerate(parents):
+          parent = parents[(i + 1) % lparents]
+          ch = crossover(p, parent, lparents)
+          parent['solution'] = ch
+
+
+        mutate(newpop, npop * numvar, mrate)
+
+ga(combination, 6, 1, 4, 0, 30, 10, 0.25)
+
