414c036e058a4f1d276a58ee4d9663d5d2d1c4be
Luis Gustavo <luisgustavomacedo13@gmail.com>
2025-01-26 13:15:44 -0300

    v1

--- simple-math-eq.py
+++ simple-math-eq.py
@@ -1,12 +1,13 @@
 import numpy as np
 import copy
+import matplotlib.pyplot as plt
 
 def combination(a, b, c, d):
     return abs((a + 2*b + 3*c + 4*d) - 30)
 
 def roullette_wheel_selection(probs, pop):
   c = np.cumsum(probs)
-  ra = np.random.rand(4)
+  ra = np.random.rand(len(pop))
   newpop = {}
   for i, r in enumerate(ra):
     w = np.argwhere(r <= c)
@@ -28,16 +29,35 @@ def crossover(p1, p2, l):
   r = np.random.randint(0, l - 1) + 1
   return np.concatenate([p1['solution'][:r], p2['solution'][r:]])
 
-def mutate(population, npop, numvar, mrate):
-  totalgen = npop + numvar
-  rate = round(totalgen * rate, 0)
+def mutate(population, npop, numvar, mrate, minbound, maxbound):
+  totalgen = npop * numvar
+  rate = int(totalgen * mrate)
   
-  for _ in rate:
+  for a in range(rate):
     r = np.random.randint(1, totalgen)
-    ind = round(r / numvar)
+    ind = r // numvar
     gen = r % numvar
-    population[ind]['solution'][gen] = np.random.randint(0, 30)
+    population[ind]['solution'][gen] = np.random.randint(minbound, maxbound)
 
+def getmostfitness(p, costfunc):
+  cost = -1
+  for i in range(len(p)):
+      ch = p[i]['solution']
+      c = costfunc(ch[0], ch[1], ch[2], ch[3])
+      if cost == -1 or c < cost:
+        cost = c
+
+  return cost
+
+def plot_solution_decrease(best_solutions, title="Best Solution Decrease Over Iterations"):
+    plt.figure(figsize=(10, 6))
+    plt.plot(range(1, len(best_solutions) + 1), best_solutions, marker='o', color='g', label='Best Solution')
+    plt.xlabel('Iteration', fontsize=12)
+    plt.ylabel('Solution Value', fontsize=12)
+    plt.title(title, fontsize=14)
+    plt.grid(True, linestyle='--', alpha=0.7)
+    plt.legend(fontsize=10)
+    plt.show()
 
 def ga(
     costfunc, 
@@ -51,10 +71,11 @@ def ga(
     mrate
 ):
     population = {}
+    bestsolutions = []
 
     for i in range(npop):
         population[i] = {
-            'cost': -1, 'solution': np.random.uniform(minbound, maxbound, numvar),
+            'cost': -1, 'solution': np.random.randint(minbound, maxbound, numvar),
         }
 
     for it in range(generations):
@@ -83,8 +104,13 @@ def ga(
           ch = crossover(p, parent, lparents)
           parent['solution'] = ch
 
+        mutate(newpop, npop, numvar, mrate, minbound, maxbound)
 
-        mutate(newpop, npop * numvar, mrate)
+        mostfitness = getmostfitness(newpop, costfunc)
+        bestsolutions.append(mostfitness)
 
-ga(combination, 6, 1, 4, 0, 30, 10, 0.25)
+    bestsolutions.reverse()
+    plot_solution_decrease(bestsolutions)
+
+ga(combination, 6, 1, 4, 0, 30, 50, 0.25, 0.1)
 
