e9e9f10c54be0ee503f22ec753788d7089c6061c
Luis Gustavo <luisgustavomacedo13@gmail.com>
2025-03-04 09:43:05 -0300

    relative path on analyzer

--- research/instance_analyzer.py
+++ research/instance_analyzer.py
@@ -1,7 +1,9 @@
 import pathlib
+import os
 from wfcommons import Instance, InstanceAnalyzer
 
-INSTANCES_PATH = pathlib.Path('path')
+d = os.path.dirname(os.path.realpath(__file__))
+INSTANCES_PATH = pathlib.Path(d, '..', 'dag-instances', 'wfcommons')
 instance_files = [f for f in INSTANCES_PATH.glob('*') if INSTANCES_PATH.joinpath(f).is_file()]
 
 analyzer = InstanceAnalyzer()
@@ -10,11 +12,8 @@ for instance_file in instance_files:
     instance = Instance(input_instance=INSTANCES_PATH.joinpath(instance_file))
     analyzer.append_instance(instance)
 
-# list of workflow task name prefixes to be analyzed in each instance
 workflow_tasks = ["split_", "blastall_", "cat_"]
 
-# building the instance summary
 instances_summary = analyzer.build_summary(workflow_tasks, include_raw_data=True)
 
-# generating all fit plots (runtime, and input and output files)
 analyzer.generate_all_fit_plots(outfile_prefix='fits/')
