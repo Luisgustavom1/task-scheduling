b96373ce9b0db1a470e543e1a2eef0ef6968e704
Luis Gustavo <luisgustavomacedo13@gmail.com>
2025-04-20 22:26:58 -0300

    move build tasks and resources to inside of simulator

--- research/main.py
+++ research/main.py
@@ -7,46 +7,11 @@ from simulator import Task, Resource, Simulator
 d = os.path.dirname(os.path.realpath(__file__))
 path = pathlib.Path(d, '..', 'dag-instances', 'wfcommons')
 instance = wfinstances.Instance(input_instance=path.joinpath("blast-chameleon-large-002.json"))
-workflow = instance.workflow
 
-tasks: list[Task] = []
-
-for task_id in workflow:
-  task: common.Task = workflow.tasks[task_id]
-  # TODO: find a better way to get IDs
-  children = [int(child.split('_ID')[1])-1 for child in workflow.tasks_children[task_id]]
-  parents = [int(parent.split('_ID')[1])-1 for parent in workflow.tasks_parents[task_id]]
-
-  ID = int(task_id.split('_ID')[1])-1
-
-  tasks.append(
-    Task(
-      ID,
-      task.name, 
-      task.runtime,
-      parents, 
-      children,
-      ID if task.priority is None else task.priority, 
-      task.cores,
-    )
-  )
-
-resources: list[Resource] = []
-
-for resource_id in instance.machines:
-  resource = instance.machines[resource_id]
-  resources.append(
-    Resource(
-      resource_id,
-      resource.memory,
-      resource.cpu_cores
-    )
-  )
-
-simulator = Simulator(tasks, resources)
-scheduler = FIFOScheduler()
+simulator = Simulator(instance)
+scheduler = HEFTScheduler()
 simulator.start(scheduler)
 print(simulator.time)
 
-# 143146.052228
-# 144182.946722
+# 143146.052228 -> HEFT
+# 144182.946722 -> FIFO
--- research/simulator.py
+++ research/simulator.py
@@ -1,5 +1,7 @@
+from wfcommons import common, wfinstances
+
 class Task:
-  def __init__(self, id, name, runtime, parents, children, priority, cores):
+  def __init__(self, id, name, runtime, parents, children, priority, cores, input_files: common.File, output_files: common.File):
     self.id = id
     self.name = name
     self.runtime = runtime
@@ -9,9 +11,12 @@ class Task:
     self.priority = priority
     self.start_time = None
     self.finish_time = None
+    self.input_files: common.File = input_files
+    self.output_files: common.File = output_files
 
 class Resource:
-  def __init__(self, name, memory, core):
+  def __init__(self, id, name, memory, core):
+    self.id = id
     self.name = name
     self.memory = memory
     self.core = core
@@ -19,14 +24,75 @@ class Resource:
     self.tasks = []
 
 class Simulator:
-  def __init__(self, tasks, resources):
-    self.tasks: list[Task] = tasks
-    self.resources: list[Resource] = resources
+  def __init__(self, instance: wfinstances.Instance):
+    self.instance: wfinstances.Instance = instance
+    self.workflow: common.Workflow = instance.workflow
+
+    self.tasks: list[Task] = []
+    self.resources: list[Resource] = []
     self.completed_tasks: list[Task] = []
     self.time = 0
 
-    self.start_task = [task for task in tasks if len(task.parents) == 0][0] 
-    self.exit_task = [task for task in tasks if len(task.children) == 0][0]
+    self.calcAverageTransferRate()
+    self.populateResources()
+    self.populateTasks()
+
+    self.start_task = [task for task in self.tasks if len(task.parents) == 0][0] 
+    self.exit_task = [task for task in self.tasks if len(task.children) == 0][0]
+
+    self.average_transfer_rate = -1
+
+  def populateTasks(self):
+    for task_id in self.workflow.nodes:
+      task: common.Task = self.workflow.tasks[task_id]
+      # TODO: find a better way to get IDs
+      children = [int(child.split('_ID')[1])-1 for child in self.workflow.tasks_children[task_id]]
+      parents = [int(parent.split('_ID')[1])-1 for parent in self.workflow.tasks_parents[task_id]]
+
+      ID = int(task_id.split('_ID')[1])-1
+
+      self.tasks.append(
+        Task(
+          ID,
+          task.name, 
+          task.runtime,
+          parents, 
+          children,
+          None,
+          task.cores,
+          task.input_files,
+          task.output_files
+        )
+      )
+
+  def populateResources(self):
+    i = 0
+    for resource_id in self.instance.machines:
+      resource = self.instance.machines[resource_id]
+      self.resources.append(
+        Resource(
+          i,
+          resource_id,
+          resource.memory,
+          resource.cpu_cores
+        )
+      )
+      i += 1
+
+  def calcAverageTransferRate(self):
+    all_data_transfer = 0
+    for edge in self.workflow.edges:
+      source = edge[0]
+      target = edge[1]
+
+      source_t: common.Task = self.workflow.tasks[source]
+      target_t: common.Task = self.workflow.tasks[target]
+
+      out_transfer = sum([file.size for file in source_t.output_files])
+      in_transfer = sum([file.size for file in target_t.input_files])
+      all_data_transfer += out_transfer + in_transfer
+
+    self.average_transfer_rate = all_data_transfer / len(self.instance.machines)
 
   def start(self, scheduler):
     scheduled_tasks: list[Task] = scheduler.schedule(self)
